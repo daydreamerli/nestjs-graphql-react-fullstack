84988d1d03ef0b7133ff523b988bf6a8ccee7a4a
frank <frankli84@outlook.com>
2021-07-18 20:07:56 -0700

    enable cros and validationpipe

--- nestjs-cars-app/src/main.ts
+++ nestjs-cars-app/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(9000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
