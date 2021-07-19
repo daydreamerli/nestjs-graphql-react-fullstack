984c3d62790c85ca4811032dc4d0fb652b3783e9
frank <frankli84@outlook.com>
2021-07-18 20:04:29 -0700

    git commit -m " setting up the graphql "

--- nestjs-cars-app/src/app.module.ts
+++ nestjs-cars-app/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ComponentsModule } from './components/components.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot({
+      playground: true,
+      debug: true,
+      autoSchemaFile: true,
+    }),
+    ComponentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
