572cd923b9aac5882bbc58d447759068e523e8ea
Frank Li <42171516+daydreamerli@users.noreply.github.com>
2021-07-22 02:34:09 -0700

    update migration

--- nestjs-cars-app/src/migrations/1622304168366-CreateDatabase.ts
+++ nestjs-cars-app/src/migrations/1622304168366-CreateDatabase.ts
@@ -1,6 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateDatabase1622304168366 implements MigrationInterface {
+  
   public async up(queryRunner: QueryRunner): Promise<void> {
     queryRunner.createDatabase('testcars', true);
   }
