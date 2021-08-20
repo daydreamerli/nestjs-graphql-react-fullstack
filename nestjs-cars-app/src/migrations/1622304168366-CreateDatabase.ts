4ee258d47a6e73c026df37aecd4e37b04c2ad41b
daydreamerli <frankli84@outlook.com>
2021-08-20 20:18:37 +1200

    change DB name in migrations

--- nestjs-cars-app/src/migrations/1622304168366-CreateDatabase.ts
+++ nestjs-cars-app/src/migrations/1622304168366-CreateDatabase.ts
@@ -3,10 +3,10 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CreateDatabase1622304168366 implements MigrationInterface {
   
   public async up(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.createDatabase('testcars', true);
+    queryRunner.createDatabase('greatcars', true);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.dropDatabase('testcars', true);
+    queryRunner.dropDatabase('greatcars', true);
   }
 }
