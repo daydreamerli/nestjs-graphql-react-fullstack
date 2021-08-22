e62fb0eddae40aaea8e3a03eb10a344f1db6c875
daydreamerli <frankli84@outlook.com>
2021-08-22 20:28:51 +1200

    add UsersModule and OrdersModule to ComponentsModule

--- nestjs-cars-app/src/components/components.module.ts
+++ nestjs-cars-app/src/components/components.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
+import { OrdersModule } from './orders/orders.module';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule,UsersModule,OrdersModule],
 })
 export class ComponentsModule {}
