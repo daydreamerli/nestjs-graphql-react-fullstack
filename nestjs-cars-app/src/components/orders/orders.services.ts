799d1526358182f49bdf7145486fd300bf1a7d50
frank <frankli84@outlook.com>
2021-08-26 16:56:38 -0700

    create services methods for order resolver

--- nestjs-cars-app/src/components/orders/orders.services.ts
+++ nestjs-cars-app/src/components/orders/orders.services.ts
@@ -1,12 +1,17 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-
+import { Repository, Connection } from 'typeorm';
+import { NewOrderInput } from './dto/new-order.input';
 import { Order } from './entities/order';
+import { User } from '../users/entities/user';
+
 
 @Injectable()
 export class OrdersService {
-  constructor(@InjectRepository(Order) private orderRepository: Repository<Order>) { }
+  constructor(
+    @InjectRepository(Order)
+    private orderRepository: Repository<Order>,
+    ) {}
 
   public async getAllOrders(): Promise<Order[]> {
     
@@ -14,5 +19,39 @@ export class OrdersService {
       throw new InternalServerErrorException();
     });
   }
+
+  public async getUserOrders(ownerId:string): Promise<Order[]> {
+    // need add where sql to identify which owner is this order belong to
+    return await this.orderRepository.find({relations:['owner'],where:{owner:{id:ownerId}}}).catch((err) => {
+      throw new InternalServerErrorException();
+    });
+  }  
+
+  public async deleteAllOrders(): Promise<Boolean> {
+    
+     await this.orderRepository.delete({}).catch((err) => {
+       throw new InternalServerErrorException();
+     });
+     return true;
+  }
+
+  public async addOrder(NewOrderData: NewOrderInput): Promise<Order> {
+
+    const newOrder = this.orderRepository.create(NewOrderData);
+
+    let userid = NewOrderData.ownerId;
+    async Connection => {
+      const userRepository = Connection.getRepository(User)
+      const orderUser = await userRepository.findOne(userid)
+      console.log(`The order is placed by user :${orderUser.username}`)
+      newOrder.owner = orderUser
+    }
+    
+    await this.orderRepository.save(newOrder).catch((err) => {
+      new InternalServerErrorException();
+    });
+    return newOrder;
+  }
+
 
 }
