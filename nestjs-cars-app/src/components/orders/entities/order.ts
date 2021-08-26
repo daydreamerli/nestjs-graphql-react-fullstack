ea11c1d2181ff6f09e6b48befb06c3381070304b
frank <frankli84@outlook.com>
2021-08-26 16:37:29 -0700

    create ManytoOne with User & OnetoMany with Car

--- nestjs-cars-app/src/components/orders/entities/order.ts
+++ nestjs-cars-app/src/components/orders/entities/order.ts
@@ -1,6 +1,8 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, RelationId, JoinColumn, BaseEntity, OneToMany } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne, RelationId, JoinColumn, BaseEntity, OneToMany, ManyToMany } from 'typeorm';
 import { IsDate } from 'class-validator';
+import { Car } from 'src/components/cars/entities/car';
+import { User } from 'src/components/users/entities/user';
 
 @Entity({ name: 'orders' })
 @ObjectType()
@@ -15,26 +17,33 @@ export class Order {
   @Field()
   amount: number;
 
-  @Column({nullable:true})
+
+  @Column()
   @Field()
-  pickup: string
-  
-  @Column({nullable:true})
-  @Field()
-  dropoff: string
+  startDate: string
 
   @Column()
   @Field()
   @IsDate()
-  startDate: Date
-
-  @Column()
-  @Field()
-  @IsDate()
-  endDate:Date
+  endDate:string
   
   @Column()
   @Field()
   duration: number;
+  
+  //foreignkey id for specific query
+  @Column()
+  ownerId: string
+  
+  @ManyToOne(() => User, user => user.orders, { onDelete: 'CASCADE' })
+  owner: User;
+
+  @ManyToMany(() => Car, cars => cars.orders)
+  cars:Car[]
+  
+  // to-do: need to save the new order while saving the related carId to middleware table
+  @Column()
+  @Field()
+  orderCars: number;
 
 }
