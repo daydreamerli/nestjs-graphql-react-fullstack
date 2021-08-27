20af36244dc2a53f795d16ae8d9cda9d87807dd8
frank <frankli84@outlook.com>
2021-08-27 01:02:17 -0700

    create the simple Homepage with twin

--- react-cars-app/src/App.tsx
+++ react-cars-app/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import styled from "styled-components";
+import tw from "twin.macro"
+import { Homepage } from './containers/Homepage';
+
+const AppContainer = styled.div`
+${tw`
+flex
+flex-col
+w-full
+h-full
+`};
+`
 
 function App() {
   return (
-    <div className="App">
-      Hello from redux and react!
-    </div>
+    <AppContainer>
+     <Homepage />
+    </AppContainer>
   );
 }
 
