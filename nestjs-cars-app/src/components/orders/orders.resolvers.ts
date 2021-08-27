3bcf29710cb63a0fd0b0ff45872ee147349652fb
frank <frankli84@outlook.com>
2021-08-26 19:34:38 -0700

    create order resolver for graphql

--- nestjs-cars-app/src/components/orders/orders.resolvers.ts
+++ nestjs-cars-app/src/components/orders/orders.resolvers.ts
@@ -1,5 +1,7 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Console } from 'console';
 import { OrdersService } from './orders.services';
+import { NewOrderInput } from './dto/new-order.input';
 import { Order } from './entities/order';
 
 
@@ -14,5 +16,27 @@ export class OrdersResolver {
     });
   }
 
+  @Query((returns) => [Order])
+  public async getUserOrders(@Args('ownerId') ownerId:string): Promise<Order[]> {
+    return await this.orderService.getUserOrders(ownerId).catch((err) => {
+      throw err;
+    });
+  }
+
+  @Mutation((returns) => Order)
+  public async addNewOrder(
+    @Args('newOrderData') newOrderData: NewOrderInput,
+  ): Promise<Order> {
+    return await this.orderService.addOrder(newOrderData).catch((err) => {
+      throw err;
+    });
+  }
+
+  @Mutation(() => Boolean)
+  public async deleteAllOrders(){
+     await this.orderService.deleteAllOrders().catch((err) => {
+      throw err;
+    });
+  }
 
 }
