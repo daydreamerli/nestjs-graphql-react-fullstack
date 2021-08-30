2166be02bf19ac03a06cd8aa6ea30cad7a90b316
frank <frankli84@outlook.com>
2021-08-29 20:21:16 -0700

    create relation query methods

--- nestjs-cars-app/src/components/orders/orders.services.ts
+++ nestjs-cars-app/src/components/orders/orders.services.ts
@@ -1,17 +1,17 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, Connection } from 'typeorm';
+import { Repository, Connection, getConnection } from 'typeorm';
 import { NewOrderInput } from './dto/new-order.input';
 import { Order } from './entities/order';
 import { User } from '../users/entities/user';
+import { Car } from '../cars/entities/car';
 
 
 @Injectable()
 export class OrdersService {
   constructor(
     @InjectRepository(Order)
-    private orderRepository: Repository<Order>,
-    ) {}
+    private orderRepository: Repository<Order>) {}
 
   public async getAllOrders(): Promise<Order[]> {
     
@@ -20,7 +20,18 @@ export class OrdersService {
     });
   }
 
-  public async getUserOrders(ownerId:string): Promise<Order[]> {
+  public async getCarOrders(id: string): Promise<Order[]>{
+    return await getConnection()
+    .createQueryBuilder()
+    .relation(Car, "orders")
+    .of(id)
+    .loadMany().catch((err) => {
+      throw new InternalServerErrorException();
+    });
+  }
+
+  public async getUserOrders(ownerId: string): Promise<Order[]> {
+
     // need add where sql to identify which owner is this order belong to
     return await this.orderRepository.find({relations:['owner'],where:{owner:{id:ownerId}}}).catch((err) => {
       throw new InternalServerErrorException();
@@ -38,20 +49,24 @@ export class OrdersService {
   public async addOrder(NewOrderData: NewOrderInput): Promise<Order> {
 
     const newOrder = this.orderRepository.create(NewOrderData);
+    // to-do: achieve manytoone and manytomany relation connection
+    // await getConnection()
+    // .createQueryBuilder()
+    //   .relation(Order, "cars")
+    //   .of(newOrder)
+    //   .add({ carId1,carId2 ...})
 
-    let userid = NewOrderData.ownerId;
-    async Connection => {
-      const userRepository = Connection.getRepository(User)
-      const orderUser = await userRepository.findOne(userid)
-      console.log(`The order is placed by user :${orderUser.username}`)
-      newOrder.owner = orderUser
-    }
+    let userId = NewOrderData.ownerId;
+    await getConnection()
+    .createQueryBuilder()
+      .relation(Order, "owner")
+      .of(newOrder)
+      .set({userId})
     
     await this.orderRepository.save(newOrder).catch((err) => {
       new InternalServerErrorException();
     });
     return newOrder;
   }
-
 
 }
