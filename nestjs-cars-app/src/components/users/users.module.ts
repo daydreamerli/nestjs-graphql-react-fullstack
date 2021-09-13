8a478eabbc7b88558cb9dbd93bcc09abd66d046b
daydreamerli <frankli84@outlook.com>
2021-09-13 22:01:43 +1200

    update user module with jwt utils and settings

--- nestjs-cars-app/src/components/users/users.module.ts
+++ nestjs-cars-app/src/components/users/users.module.ts
@@ -3,10 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.services';
 import { User } from './entities/user';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.stratage';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, UsersResolver],
+  imports: [TypeOrmModule.forFeature([User]), JwtModule.register({
+    secret: 'hard!to-guess_secret',
+    signOptions: { expiresIn: '1h' },
+  }),],
+  providers: [UsersService, UsersResolver,JwtStrategy],
   exports: [UsersService],
 })
 export class UsersModule {}
