305839e9cc01391d6dbe1e295918e3651760495f
daydreamerli <frankli84@outlook.com>
2021-09-15 23:27:55 +1200

    set up authorization for graphql client

--- react-cars-app/src/app/graphql.ts
+++ react-cars-app/src/app/graphql.ts
@@ -1,6 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: `${process.env.REACT_APP_API_URL}/graphql`,
+});
+
+const authLink = setContext((_, { headers }) => {
+   // get the authentication token from local storage if it exists
+   const token = localStorage.getItem('token');
+   // return the headers to the context so httpLink can read them
+   return {
+     headers: {
+       ...headers,
+       authorization: token ? `Bearer ${token}` : "",
+     }
+   }
+});
 
 export const apolloClient = new ApolloClient({
-  uri: `${process.env.REACT_APP_API_URL}/graphql` as any,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
