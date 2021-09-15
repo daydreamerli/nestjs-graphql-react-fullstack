782dee577da06d716a759ac516d790ab306f7039
daydreamerli <frankli84@outlook.com>
2021-09-15 23:34:22 +1200

    adding cros

--- nestjs-cars-app/src/main.ts
+++ nestjs-cars-app/src/main.ts
@@ -4,10 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // create validation pipeline for entities
   app.useGlobalPipes(new ValidationPipe());
-
+  //enable cros for jwt athorization
   app.enableCors();
 
   await app.listen(process.env.PORT || 9000);
 }
 bootstrap();
+
+
+
