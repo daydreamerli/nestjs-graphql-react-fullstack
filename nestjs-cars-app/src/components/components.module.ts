42324dd37cb41d11b9c7898f7e07847797156516
daydreamerli <frankli84@outlook.com>
2021-09-28 18:40:33 +1300

    auth module included

--- nestjs-cars-app/src/components/components.module.ts
+++ nestjs-cars-app/src/components/components.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [CarsModule,UsersModule,OrdersModule],
+  imports: [CarsModule,UsersModule,OrdersModule, AuthModule],
 })
 export class ComponentsModule {}
