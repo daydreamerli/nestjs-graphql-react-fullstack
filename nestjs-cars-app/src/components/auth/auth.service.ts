331cf881d5487ed39f7a174b4b488c46a1e49fd6
daydreamerli <frankli84@outlook.com>
2021-09-28 18:55:50 +1300

    create auth service with validation login signup functions

--- nestjs-cars-app/src/components/auth/auth.service.ts
+++ nestjs-cars-app/src/components/auth/auth.service.ts
@@ -1,4 +1,60 @@
-import { Injectable } from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {JwtService} from '@nestjs/jwt'
+import { User } from '../users/entities/user';
+import { UsersService } from '../users/users.services';
+import { jwtSecret } from './constants';
+import * as bcrypt from 'bcrypt';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { NewUserInput } from '../../components/users/dto/new-user.input';
 
+@ObjectType()
+export class access_token{
+  @Field()
+  access_token:string;
+}
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,) {}
+    
+  async validate(email: string, password: string): Promise<User | null> {
+    
+    const user = await this.userService.findByUserEmail(email)
+    console.log(`User with email : ${email} is trying to login`)
+    if (!user) {
+      return null;
+    }
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+    console.log('The user input password is valid :', passwordIsValid)
+    if (!passwordIsValid) {
+      return null;
+    } else {
+       
+      return user;
+    }
+  }
+
+  async login(user: User): Promise<access_token> {
+  
+    const payload = { id: user.id, email:user.email }
+    return{ access_token:this.jwtService.sign(payload)}
+  }
+
+  async signup(NewUserDate:NewUserInput): Promise<User> {
+  
+    return await this.userService.createUser(NewUserDate);
+  }
+
+  async verify(token: string): Promise<User | any> {
+
+    const decoded = this.jwtService.verify(token, { secret: jwtSecret });
+    const user = await this.userService.findByUserEmail(decoded.email)
+
+    if (!user) {
+      throw new Error('Unable to get the user from the token service!')
+    }
+
+    return user;
+  }  
+}
