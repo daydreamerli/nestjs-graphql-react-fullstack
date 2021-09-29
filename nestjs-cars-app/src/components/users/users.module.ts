c296adc2b336b244eeeed781c9e1588b144cc4e7
daydreamerli <frankli84@outlook.com>
2021-09-29 21:45:38 +1300

    add authmodule with forwarRef to userModule

--- nestjs-cars-app/src/components/users/users.module.ts
+++ nestjs-cars-app/src/components/users/users.module.ts
@@ -1,17 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.services';
 import { User } from './entities/user';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.stratage';
+import { AuthModule } from '../auth/auth.module';
+
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), JwtModule.register({
-    secret: 'hard!to-guess_secret',
-    signOptions: { expiresIn: '1h' },
-  }),],
-  providers: [UsersService, UsersResolver,JwtStrategy],
+  imports: [TypeOrmModule.forFeature([User]),forwardRef(() => AuthModule)
+  ],
+  providers: [UsersService, UsersResolver],
   exports: [UsersService],
 })
 export class UsersModule {}
