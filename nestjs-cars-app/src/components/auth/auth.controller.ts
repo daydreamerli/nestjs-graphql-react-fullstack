1c11fa85039ca22c9c65575268211c9e4d03b7dc
daydreamerli <frankli84@outlook.com>
2021-09-29 21:30:48 +1300

    wrap up controller

--- nestjs-cars-app/src/components/auth/auth.controller.ts
+++ nestjs-cars-app/src/components/auth/auth.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { User } from '../users/entities/user';
+import { AuthService, access_token } from './auth.service';
+import { LocalAuthGuard } from './guards/local-host.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  constructor(private readonly authService: AuthService) { }
+  
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Req() req: Request): Promise<access_token> {
+     
+    return this.authService.login(req.user as User);
+  }
+
+  @Post('signup')
+  async signup(@Req() req: Request): Promise<User> {
+     
+    return this.authService.signup(req.body);
+  }
+
+}
