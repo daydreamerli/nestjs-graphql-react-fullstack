ed8c0a57a794287c5a43c25199d4ff71a175bbd9
daydreamerli <frankli84@outlook.com>
2021-09-29 21:59:41 +1300

    create car page for different categories

---
+++ react-cars-app/src/app/containers/CarPage/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Footer } from "../../components/footer";
+import { Marginer } from "../../components/marginer";
+import { Navbar } from "../../components/navbar";
+import { TopSUV } from "./topSUV";
+import { TopSection } from "./topSection";
+import { TopHatchBack } from "./topHatchBack";
+import { TopHybird } from "./topHybird";
+
+const PageContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `}
+`;
+
+export function CarPage() {
+  return (
+    <PageContainer>
+      <Navbar />
+      <TopSection />
+      <TopSUV />
+      <TopHatchBack />
+      <TopHybird />
+      <Marginer direction="vertical" margin="2em" />
+      <Footer />
+    </PageContainer>
+  );
+}
