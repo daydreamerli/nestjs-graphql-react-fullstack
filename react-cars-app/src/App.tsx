14226e13b71f6dd7a504697bfe116629d0591a1b
daydreamerli <frankli84@outlook.com>
2021-10-02 16:06:19 +1300

    update route in app

--- react-cars-app/src/App.tsx
+++ react-cars-app/src/App.tsx
@@ -3,6 +3,12 @@ import styled from "styled-components";
 import tw from "twin.macro";
 import "./App.css";
 import { HomePage } from "./app/containers/HomePage";
+import { Route, BrowserRouter as Router, } from "react-router-dom";
+import { CarPage } from "./app/containers/CarPage";
+import { LoginPage } from "./app/containers/UserLogin";
+import { SignupPage } from "./app/containers/signupPage";
+import { OrderPage } from "./app/containers/orderPage";
+
 
 const AppContainer = styled.div`
   ${tw`
@@ -13,12 +19,24 @@ const AppContainer = styled.div`
   `};
 `;
 
+
 function App() {
+
   return (
     <AppContainer>
-      <HomePage />
+      <Router>
+        <switch>
+          <Route path="/" exact component={() => <HomePage />} />
+          <Route path="/cars" exact component={() => <CarPage />} />
+          <Route path="/order" exact component={() => <OrderPage />} />
+          <Route path="/login" exact component={() => <LoginPage />} />
+          <Route path="/signup" exact component={() => <SignupPage />} />
+        </switch>
+      </Router>
     </AppContainer>
   );
 }
 
 export default App;
+
+
