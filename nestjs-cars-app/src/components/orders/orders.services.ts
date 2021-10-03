5638d4afdc8f8a8cdd65731ce2fb7bc3c33c1772
daydreamerli <frankli84@outlook.com>
2021-10-03 20:52:37 +1300

    add commit in newOrder section

--- nestjs-cars-app/src/components/orders/orders.services.ts
+++ nestjs-cars-app/src/components/orders/orders.services.ts
@@ -1,19 +1,19 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository, Connection, getConnection } from 'typeorm';
+import { InjectRepository,InjectEntityManager, TypeOrmModule } from '@nestjs/typeorm';
+import { Connection, Repository ,getConnection,getConnectionManager,getRepository} from 'typeorm';
 import { NewOrderInput } from './dto/new-order.input';
+import { UpdateOrderInput } from './dto/update-order.input';
 import { Order } from './entities/order';
 import { User } from '../users/entities/user';
 import { Car } from '../cars/entities/car';
 
 
-@Injectable()
+@Injectable() 
 export class OrdersService {
   constructor(
     @InjectRepository(Order)
     private orderRepository: Repository<Order>) {}
-  
-  //only open for car management admin user
+
   public async getAllOrders(): Promise<Order[]> {
     
     return await this.orderRepository.find({}).catch((err) => {
@@ -29,44 +29,44 @@ export class OrdersService {
     .loadMany().catch((err) => {
       throw new InternalServerErrorException();
     });
+   
   }
-  
-  public async getUserOrders(ownerId: string): Promise<Order[]> {
 
-    // need add where sql to identify which owner is this order belong to
+  public async getUserOrders(ownerId: string): Promise<Order[]> {
+  
     return await this.orderRepository.find({relations:['owner'],where:{owner:{id:ownerId}}}).catch((err) => {
       throw new InternalServerErrorException();
     });
-  }  
-
+  }
+  
   public async deleteAllOrders(): Promise<Boolean> {
-    
+  
      await this.orderRepository.delete({}).catch((err) => {
        throw new InternalServerErrorException();
      });
      return true;
   }
 
-  public async addOrder(NewOrderData: NewOrderInput): Promise<Order> {
-
+  public async addOrder(NewOrderData: NewOrderInput, carsId: string): Promise<Order> {
+    
+    const userId = NewOrderData.ownerId;
+    // covert the simple string to carId array
+    const carsIdArray = carsId.split(",")
     const newOrder = this.orderRepository.create(NewOrderData);
-    // to-do: achieve manytoone and manytomany relation connection
-    // await getConnection()
-    // .createQueryBuilder()
-    //   .relation(Order, "cars")
-    //   .of(newOrder)
-    //   .add({ carId1,carId2 ...})
-
-    let userId = NewOrderData.ownerId;
+    await this.orderRepository.save(newOrder);
+    // achieve manytoone and manytomany relation connection
     await getConnection()
-    .createQueryBuilder()
+      .createQueryBuilder()
+      .relation(Order, "cars")
+      .of(newOrder)
+      .add(carsIdArray);
+  
+    await getConnection()
+      .createQueryBuilder()
       .relation(Order, "owner")
       .of(newOrder)
-      .set({userId})
-    
-    await this.orderRepository.save(newOrder).catch((err) => {
-      new InternalServerErrorException();
-    });
+      .set(userId);
+  
     return newOrder;
   }
 
